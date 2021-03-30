f3edb28678407a60c95951d0ab68f90d62f3b5f3
Andres Zapata <azapata@uc.edu.co>
2021-03-29 21:58:48 -0500

    Observables subjects

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import { BehaviorSubject } from 'rxjs';
+import { take, tap } from'rxjs/operators';
 
 
 const QUERY= gql`
@@ -36,8 +37,29 @@ const QUERY= gql`
 export class DataService {
 
   private episodeSubject = new BehaviorSubject<any[]>(null);
+  episodes$ = this.episodeSubject.asObservable();
 
-  episodeSubject
+  private charactersSubject = new BehaviorSubject<any[]>(null);
+  characters$ = this.charactersSubject.asObservable();
 
-  constructor() { }
+
+  
+  constructor(private apollo: Apollo) { 
+
+    this.getDataApi();
+  }
+
+
+
+  private getDataApi():void{
+    this.apollo.watchQuery<any>({
+      query: QUERY
+    }).valueChanges.pipe(
+        take(1),
+        tap(res=>{
+          console.log(res);
+          
+        })
+    ).subscribe();
+  }
 }
